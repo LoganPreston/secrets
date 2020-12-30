6e3c2e83afb1fb9e477acd7afb561918cec4b5a2
logan preston <personal@loganpreston.com>
2020-12-29 19:36:13 -0600

    add login and register functionality. store username/pw in mongoDB in plain text

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //required modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 //express and ejs setup
 const app = express();
@@ -10,16 +11,73 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//mongoose setup
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("user", userSchema);
+
+//GET requests
+
+//home
 app.get("/", function (req, res) {
   res.render("home");
 });
 
+//login
 app.get("/login", function (req, res) {
   res.render("login");
 });
 
+//register
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+//POST requests
+
+//login
+app.post("/login", function (req, res) {
+  User.findOne(
+    { username: req.body.username, password: req.body.password },
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/login");
+      }
+      //no error
+      else {
+        if (user) {
+          res.render("secrets");
+        } else {
+          res.redirect("/login");
+        }
+      }
+    }
+  );
+});
+
+//register
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      res.render("secrets");
+    }
+  });
 });
 
 //listen on to port 3000
